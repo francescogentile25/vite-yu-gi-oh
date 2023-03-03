0f4f59921b4c040f96006e44a9f2eaa14a8eec8c
francescogentile25 <francescogentile250893@gmail.com>
2023-03-03 14:45:59 +0100

    FEAT add scaffolding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Main from './components/Main.vue';
+export default {
+  components: {
+    Main,
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<template>
+  <Main></Main>
+</template>
+
+<style lang="scss">
+@use './style/general.scss'
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Ciao</h1>
+    </div>
+    <Card></Card>
+</template>
+
+<script>
+import Card from './Card.vue'
+export default {
+    components: {
+        Card,
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<style lang="scss" scoped></style>
