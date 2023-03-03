d880c2146c70afd227dc56bd60043f0ed65c1fd3
francescogentile25 <francescogentile250893@gmail.com>
2023-03-03 16:26:58 +0100

    FEAT add cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,8 @@
 <template>
     <div>
         <li v-for="(card, i)  in cards" key="i">
-            prova
+            <img :src="card.card_images[0].image_url" alt="">
+            <p>{{ card.name }}</p>
         </li>
     </div>
     <Card></Card>
@@ -25,7 +26,7 @@ export default {
     methods: {
         fetchCard() {
             axios
-                .get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+                .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
                 .then((res) => {
                     console.log(res.data.data)
                     this.cards = res.data.data
