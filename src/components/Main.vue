facb362f93e4fc72766da498aeca5e78bd6a93cf
francescogentile25 <francescogentile250893@gmail.com>
2023-03-03 16:34:52 +0100

    STYLE add boostrap card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,18 @@
 
 <template>
-    <div>
+    <!-- <div>
         <li v-for="(card, i)  in cards" key="i">
             <img :src="card.card_images[0].image_url" alt="">
             <p>{{ card.name }}</p>
         </li>
+        </div> -->
+
+    <div class="card" style="width:300px" v-for="(card, i)  in cards" key="i">
+        <img :src="card.card_images[0].image_url" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h3 class="card-text text-center">{{ card.name }}</h3>
+            <p class="card-text text-center">{{ card.type }}</p>
+        </div>
     </div>
     <Card></Card>
 </template>
